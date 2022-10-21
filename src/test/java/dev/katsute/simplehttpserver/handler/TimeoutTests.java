0e4c1cb6bb526ecbe43c260a94a8c6b965a4d652
Katsute <58778985+Katsute@users.noreply.github.com>
2022-10-20 21:10:41 -0400

    Add timeout handler (#17)

---
+++ src/test/java/dev/katsute/simplehttpserver/handler/TimeoutTests.java
@@ -0,0 +1,88 @@
+package dev.katsute.simplehttpserver.handler;
+
+import dev.katsute.simplehttpserver.*;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+final class TimeoutTests {
+
+    private static SimpleHttpServer server;
+
+    @BeforeAll
+    static void beforeAll() throws IOException{
+        server = SimpleHttpServer.create(8080);
+        server.start();
+    }
+
+    @AfterAll
+    static void afterAll(){
+        server.stop();
+    }
+
+    @Test
+    final void testTimeout(){
+        server.createContext("timeout/timeout", new TimeoutHandler(
+            (SimpleHttpHandler) exchange -> {
+                try{
+                    Thread.sleep(4000);
+                    exchange.send(200);
+                }catch(InterruptedException ignored){}
+            },
+            2,
+            TimeUnit.SECONDS
+        ));
+
+        final AtomicInteger code = new AtomicInteger();
+
+        Assertions.assertTimeout(
+            Duration.of(3, ChronoUnit.SECONDS),
+            () -> code.set(Requests.getCode("http://localhost:8080/timeout/timeout"))
+        );
+
+        Assertions.assertEquals(408, code.get());
+    }
+
+    @Test
+    final void testTimeoutSeconds(){
+        server.createContext("timeout/nou", new TimeoutHandler(
+            (SimpleHttpHandler) exchange -> {
+                try{
+                    Thread.sleep(4000);
+                    exchange.send(200);
+                }catch(InterruptedException ignored){}
+            },
+            2
+        ));
+
+        final AtomicInteger code = new AtomicInteger();
+
+        Assertions.assertTimeout(
+            Duration.of(3, ChronoUnit.SECONDS),
+            () -> code.set(Requests.getCode("http://localhost:8080/timeout/nou"))
+        );
+
+        Assertions.assertEquals(408, code.get());
+    }
+
+    @Test
+    final void testTimeoutPass(){
+        server.createContext("timeout/pass", new TimeoutHandler(
+            (SimpleHttpHandler) exchange -> {
+                try{
+                    Thread.sleep(4000);
+                    exchange.send(200);
+                }catch(InterruptedException ignored){}
+            },
+            5,
+            TimeUnit.SECONDS
+        ));
+
+        Assertions.assertEquals(200, Requests.getCode("http://localhost:8080/timeout/pass"));
+    }
+
+}
